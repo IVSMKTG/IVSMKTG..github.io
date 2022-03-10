5fe2abda547dee6a79bafe03e3ca22674c89242e
IVSMKTG <101366727+IVSMKTG@users.noreply.github.com>
2022-03-11 11:03:59 +1300

    My first commit!

---
+++ accredited-person-refresher-online-course-scorm12-lmzEPYWp/scormcontent/assets/ihv8VaN_WvepLCBb/story_content/5qvSu4Qn42a_44100_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.36.21213.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.082%0D%0AFrogs,%20toads,%20lizards,%20snakes.%0D%0A%0D%0A"}')
---
+++ accredited-person-refresher-online-course-scorm12-lmzEPYWp/scormcontent/assets/ihv8VaN_WvepLCBb/story_content/5rTP5P9jP49_44100_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.36.21213.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.545%0D%0ACaused%20by%20the%20fungus%20Fusarium%20circinatum,%20a%20serious%20disease%20of%20pines.%0D%0A%0D%0A"}')
